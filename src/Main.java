9a5cc784ec0b569bc3e7d20105accfb8890b6d4f
Luis Fernando Machado <>
2023-08-03 22:29:17 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import db.DB;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+
+        Connection conn = DB.getConnection();
+
+        Statement st = conn.createStatement();
+
+        ResultSet rs = st.executeQuery("select * from tb_product");
+
+        while (rs.next()) {
+            System.out.println(rs.getLong("Id") + ", " + rs.getString("Name"));
+        }
+    }
+}
