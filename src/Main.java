c4ff84f33af1bc9ad8143e2ba38cd826bb4bbeee
Luis Fernando Machado <>
2023-08-04 21:53:24 -0300

    I reviewed fundamentals concept about of OOP, SQL and Connection on Postgres

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,8 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
@@ -19,9 +21,30 @@ public class Main {
                 "INNER JOIN tb_order_product ON tb_order.id = tb_order_product.order_id " +
                 "INNER JOIN tb_product ON tb_product.id = tb_order_product.product_id");
 
+        Map<Long, Order> orders = new HashMap<>();
+        Map<Long, Product> products = new HashMap<>();
         while (rs.next()) {
-            Order order = instantiateOrder(rs);
-            System.out.println(order);
+            Long orderId = rs.getLong("order_id");
+            if (orders.get(orderId) == null) {
+                Order order = instantiateOrder(rs);
+                orders.put(orderId, order);
+            }
+
+            Long productId = rs.getLong("product_id");
+            if (products.get(productId) == null) {
+                Product product = instantiateProduct(rs);
+                products.put(productId, product);
+            }
+            orders.get(orderId).getProducts().add(products.get(productId));
+        }
+
+        for (Long orderId : orders.keySet()) {
+            Order order = orders.get(orderId);
+            System.out.println("\nOrder ID: " + order.getId() + " | At Moment: " + order.getMoment() + " | Status: " + order.getStatus());
+
+            for (Product product : order.getProducts()) {
+                System.out.println("\t"+ product.getDescription());
+            }
         }
     }
     private static Order instantiateOrder(ResultSet rs) throws SQLException {
